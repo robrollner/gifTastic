94e78b24b49a3c4f9e71b0bec6b65b5f444c8f57
Robert Rollner <robrollner@gmail.com>
2017-08-27 13:10:25 -0600

    adding pseudocode of non working source changing function

--- assets/javascript/gifJavaScript.js
+++ assets/javascript/gifJavaScript.js
@@ -1,9 +1,9 @@
 $(document).ready(function() {
-
+//array of characters
     var characters = ["Bart Simpson", "Deadpool", "Dr Nick", "Doctor Who", "Philip J Fry", "Bender B Rodriguez", "Moe Szyslak", "Macho Man", "Dory", "Dug"];
     console.log(characters);
 
-
+//renders character buttons
     function renderButtons() {
         $('#buttons-view').empty();
 
@@ -34,27 +34,29 @@ $(document).ready(function() {
     $("#buttons-view").on("click", ".btn-primary", function() {
 
         var person = $(this).attr("data-person");
+        //API calls retunrs 15 repsonses only 1 loads...
         var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + person + "&api_key=dc6zaTOxFJmzC&limit=15";
         console.log(queryURL);
-
+	//calls giphy API
         $.get(queryURL).done(function(response) {
             var results = response.data;
             console.log(results);
-
+        //assigned variables to still image and gif
             var stillImage = results[0].images.original_still.url
             var gifImage = results[0].images.original.url
-
+        //loads rating object to html with click on character button
             $('.rating').html('Rated: ' + results[0].rating);
-
+        //loads still image with click of character button
             $('.image').html('<img class="img-thumbnail" src="' + stillImage + ' "data-state="still"' + '>');
         });
-
-        $("img").on({
+        //supposed to add clicking event to image that changes src of still image to gif
+        $(".img-thumbnail").on({
             'click': function() {
-                var src = ($(this).attr('src') === stillImage),
-                    stillImage, 
+                var src = ($(this).attr('src') === stillImage) ?
+                    stillImage :
                     gifImage;
                 $(this).attr('src', src);
+                console.log(img-thumbnail);
 
             }
         });
